44db53c375c37d3bdd7f91a0477fb5fce8836dfc
Mamisoa <mamisoa@gmail.com>
2023-08-17 00:46:41 +0200

    change scope

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
 import os.path
 
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+# SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
+
 CRED_PATH = './env/credentials.json'
 TOKEN = './env/token.json'
 
@@ -32,7 +34,7 @@ def authenticate():
 
     return build('gmail', 'v1', credentials=creds)
 
-def main():
+def get_labels():
     """
     Authenticates the user and prints the list of Gmail labels.
 
@@ -50,5 +52,35 @@ def main():
         for label in labels:
             print(label['name'])
 
+def read_latest_unread_email(service):
+    """
+    Reads the latest unread email from the user's Gmail account.
+
+    Args:
+        service: Authorized Gmail API service client.
+
+    Returns:
+        message: A dictionary containing the details of the latest unread email.
+    """
+    # Step 1: Search for unread emails
+    results = service.users().messages().list(userId='me', q='is:unread').execute()
+    messages = results.get('messages', [])
+
+    if not messages:
+        print('No unread messages found.')
+        return None
+    else:
+        # Step 2: Get the latest unread email
+        latest_message_info = messages[0]  # Assuming the latest message is the first in the list
+        msg = service.users().messages().get(userId='me', id=latest_message_info['id']).execute()
+
+        # Step 3 (optional): Mark the email as read
+        service.users().messages().modify(userId='me', id=latest_message_info['id'], body={'removeLabelIds': ['UNREAD']}).execute()
+
+        print(f"Message Subject: {msg['subject']}")
+        return msg
+
 if __name__ == '__main__':
-    main()
+    service = authenticate()
+    message = read_latest_unread_email(service)
+    print("messsages:",message)
