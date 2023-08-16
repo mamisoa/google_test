7586115d1115a6461ffbc139b38cc4e5a88cd61b
Mamisoa <mamisoa@gmail.com>
2023-08-17 00:55:36 +0200

    get content

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
+import base64
 import os.path
 
 # SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
@@ -77,30 +78,39 @@ def read_latest_unread_email(service):
         service: Authorized Gmail API service client.
 
     Returns:
-        message: A dictionary containing the details of the latest unread email.
+        subject: Subject of the email.
+        body: Body content of the email.
     """
-    # Step 1: Search for unread emails
+    # Search for unread emails
     results = service.users().messages().list(userId='me', q='is:unread').execute()
     messages = results.get('messages', [])
 
     if not messages:
         print('No unread messages found.')
-        return None
-    else:
-        # Step 2: Get the latest unread email
-        latest_message_info = messages[0]  # Assuming the latest message is the first in the list
-        msg = service.users().messages().get(userId='me', id=latest_message_info['id']).execute()
+        return None, None
 
-        # Step 3 (optional): Mark the email as read
-        # service.users().messages().modify(userId='me', id=latest_message_info['id'], body={'removeLabelIds': ['UNREAD']}).execute()
+    latest_message_info = messages[0]
+    msg = service.users().messages().get(userId='me', id=latest_message_info['id']).execute()
 
-        subject = get_subject_from_message(msg)
-        if subject:
-            print(f"Message Subject: {subject}")
-        else:
-            print("Subject not found")
-            
-        return msg
+    # Extracting the subject
+    subject = None
+    headers = msg['payload']['headers']
+    for header in headers:
+        if header['name'] == 'Subject':
+            subject = header['value']
+
+    # Extracting the body content
+    part = msg['payload']['parts'][0]
+    body_data = part['body']['data']
+    body = base64.urlsafe_b64decode(body_data).decode('utf-8')
+
+    # Optional: Marking the email as read
+    service.users().messages().modify(userId='me', id=latest_message_info['id'], body={'removeLabelIds': ['UNREAD']}).execute()
+
+    print(f"Message Subject: {subject}")
+    print(f"Message Body: {body}")
+
+    return subject, body
 
 if __name__ == '__main__':
     service = authenticate()
